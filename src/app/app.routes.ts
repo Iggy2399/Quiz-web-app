d63246c5e44416f9f17da84a17f74a96b92069c6
Igor.Balic <igor.balic.27@gmail.com>
2023-12-12 12:24:10 +0100

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+import { LoginComponent } from './Login/login.component';
+import { PocetnaComponent } from './pocetna/pocetna.component';
+
+
+export const routes: Routes = [
+    {   
+        path : 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'pocetna',
+        component: PocetnaComponent
+        
+    },
+    {
+      path:'**',
+      redirectTo: 'login',
+      
+      
+    }
+  ];
---
+++ src/app/pocetna/pocetna.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pocetna',
+  standalone: true,
+  imports: [],
+  templateUrl: './pocetna.component.html',
+  styleUrl: './pocetna.component.css'
+})
+export class PocetnaComponent {
+
+ 
+}
