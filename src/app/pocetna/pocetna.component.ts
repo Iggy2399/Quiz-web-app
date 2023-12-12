4656eac0b153694ad6914cb227d7ec9fa4334bf0
Igor.Balic <igor.balic.27@gmail.com>
2023-12-12 14:47:19 +0100

    Api fetch JSON from url, fix display data on pocetna

--- src/app/pocetna/pocetna.component.ts
+++ src/app/pocetna/pocetna.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../../servisi/api.services';
+
+
 
 @Component({
   selector: 'app-pocetna',
@@ -10,5 +13,32 @@ import { Router } from '@angular/router';
 })
 export class PocetnaComponent {
   
-  }
+  loading = false;
+  loginVisible: boolean = true;
+  formData: any = {};
+  passwordMode: string = "password";
+  formDataReset: any = {};
+  recoveryCode: string = '';
+  sendEmailVisible: boolean = false;
+  formDataEmail: any = {};
+
+  
  
+  this._api.dohvatiPodatke({'email': this.formData.username, 'lozinka': this.password}).subscribe(res=>{
+    if(res.success == true){
+      sessionStorage.setItem('album_id', res.data.kor_ime);
+      sessionStorage.setItem('id', res.data.kor_prezime);
+      sessionStorage.setItem('title', res.data.kor_email);
+      sessionStorage.setItem('url', res.data.kor_telefon);
+      sessionStorage.setItem('thumbnailUrl', res.data.kor_aktivan);
+      this.authService.logIn(this.formData.username, this.password);
+      this.loading = false;
+    } else {
+      this.loading = false;
+      notify("Korisnik ne postoji ili nije aktivan!", "error");
+    }
+  },err => {
+    notify(err.message, "error");
+    this.loading = false;
+  });
+}
--- src/servisi/api.services.ts
+++ src/servisi/api.services.ts
@@ -1,6 +1,11 @@
 import { Component } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
 
 
 export class ApiService{
@@ -9,7 +14,7 @@ export class ApiService{
       ){}
     
     dohvatiPodatke(a: any){
-        return this._http.post('https://jsonplaceholder.typicode.com/photos',+ 'podaci'+ a)
+        return this._http.post('https://jsonplaceholder.typicode.com/photos' + 'podaci' , a)
         .pipe(map((res: any) => res ));
     }
 }
