ca1465e0956b867ef0cc1a5b9e8d66817e41bc3f
Igor.Balic <igor.balic.27@gmail.com>
2023-12-14 14:45:27 +0100

    Button disabled if no login data in fields

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, NgModule } from "@angular/core";
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
-import { FormControl, FormGroup, FormsModule } from "@angular/forms";
+import { FormControl, FormGroup, FormsModule, Validators } from "@angular/forms";
 import { ReactiveFormsModule, FormBuilder} from "@angular/forms";
 
 
@@ -27,14 +27,18 @@ export class LoginComponent{
         private fb: FormBuilder,
     ){
         this.user = new FormGroup({
-            email : new FormControl(''),
-            lozinka : new FormControl ('')
+            email : new FormControl('', [Validators.email, Validators.required]),
+            lozinka : new FormControl('', [Validators.minLength(5), Validators.maxLength(12), Validators.required])
+            
+            
 
         })             
     }
     prijava(){
         
         console.log('user data:',this.user);
+        
+        
 
         if(this.user.value.email === "admin@admin.hr" && this.user.value.lozinka === "admin"){
             this.router.navigate(['/pocetna']);
