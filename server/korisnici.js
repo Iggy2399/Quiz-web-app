27441c90705fc90f63a6b736361e7dbea0e16529
Igor.Balic <igor.balic.27@gmail.com>
2024-01-10 13:51:39 +0100

    api for login

---
+++ server/korisnici.js
@@ -0,0 +1,16 @@
+export const korisnici =[
+    {
+        ime : "Ivan Ivic",
+        email: "ivan.ivic@gmail.com",
+        lozinka: "12345",
+        admin : false
+    },
+    {
+        ime : "pero peric",
+        email: "pero.peric@gmail.com",
+        lozinka: "pero",
+        admin : false
+    }
+
+
+]
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+import { korisnici } from './korisnici';
+import { jwt } from 'jsonwebtoken';
 
 const express = require('express'); 
+const { korisnici } = require('./korisnici');
 const app = express(); 
-  
+app.use(express.json())
 // handling CORS 
 app.use((req, res, next) => { 
     res.header("Access-Control-Allow-Origin",  
@@ -21,6 +24,29 @@ app.get('/api/message', (req, res) => {
             'Poruka sa servera' }); 
 }); 
   
+
+app.post("api/users/login",(req,res)=>{
+    const body = req.body;
+    const{email, lozinka} = req.body
+    const user = korisnici.find(user => user.email === email 
+        && user.lozinka === lozinka);
+
+        if(user){
+            res.send(generateTokenResponse(user))
+        }else{
+            res.status(400).send("email ili lozinka nisu ispravni")
+        }
+
+})
+const generateTokenResponse = (user) => {
+    const token = jwt.sign({
+        email:user.email, admin:user.admin,
+        
+    },"Text", {expiresIn:"30d"});
+    user.token = token;
+    return user;
+}
 app.listen(3000, () => { 
     console.log('Server listening on port 3000'); 
 });
+
